cb313f7e4429cf4e8f83d52a125dadef15fbd9de
Your Name <you@example.com>
2017-07-16 12:38:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# node.js typescript (to ES6) project seed
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+
+class Test {
+    greetWith(name: string) {
+        console.log(`Hello ${name}`)
+    } 
+}
+
+new Test().greetWith('Stranger')
